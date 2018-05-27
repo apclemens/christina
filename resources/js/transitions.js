56913055f40b3d77f09359cecef41b3225f6a1e7
DontKnockMySmock <apclemens1@gmail.com>
2018-05-27 12:41:16 -0400

    page opacity

--- resources/js/transitions.js
+++ resources/js/transitions.js
@@ -24,35 +24,45 @@ function setAllLinks() {
         link.addEventListener('click', function(event) {
             event.preventDefault();
             httpGetAsync(this.href, this.pathname, function(href, pathname, html){
-                var innerHTML = html.split('<!-- CONTENT -->')[1];
-                var title = html.split('<title>')[1].split('</title>')[0];
-                window.history.pushState({
-                    innerHTML: innerHTML,
-                    title: title,
-                    linkText: pathname == '/' ? 'CV' : "HOME",
-                }, "", href)
-                document.getElementById('content').innerHTML = innerHTML;
-                document.title = title;
-                if (pathname == '/') {
-                    document.getElementById('link').innerHTML = '<a href="/cv">CV</a>';
-                } else {
-                    document.getElementById('link').innerHTML = '<a href="/">HOME</a>';
-                }
-                setAllLinks();
+                document.getElementById('content').style.opacity = 0;
+                setTimeout(function(){
+                    var innerHTML = html.split('<!-- CONTENT -->')[1];
+                    var title = html.split('<title>')[1].split('</title>')[0];
+                    window.history.pushState({
+                        innerHTML: innerHTML,
+                        title: title,
+                        linkText: pathname == '/' ? 'CV' : "HOME",
+                    }, "", href)
+                    document.getElementById('content').innerHTML = innerHTML;
+                    document.title = title;
+                    if (pathname == '/') {
+                        document.getElementById('link').innerHTML = '<a href="/cv">CV</a>';
+                    } else {
+                        document.getElementById('link').innerHTML = '<a href="/">HOME</a>';
+                    }
+                    document.getElementById('content').style.opacity = 1;
+                    setAllLinks();
+                }, 250)
             })
         })
     }
 }
 
 window.onpopstate = function(event) {
+    console.log(event);
     if (event.state) {
-        document.getElementById('content').innerHTML = event.state.innerHTML;
-        document.title = event.state.title;
-        if (event.state.linkText == 'CV') {
-            document.getElementById('link').innerHTML = '<a href="/cv">CV</a>';
-        } else {
-            document.getElementById('link').innerHTML = '<a href="/">HOME</a>';
-        }
+        document.getElementById('content').style.opacity = 0;
+        setTimeout(function() {
+            document.getElementById('content').innerHTML = event.state.innerHTML;
+            document.title = event.state.title;
+            if (event.state.linkText == 'CV') {
+                document.getElementById('link').innerHTML = '<a href="/cv">CV</a>';
+            } else {
+                document.getElementById('link').innerHTML = '<a href="/">HOME</a>';
+            }
+            document.getElementById('content').style.opacity = 1;
+            setAllLinks();
+        }, 250);
     }
 }
 
